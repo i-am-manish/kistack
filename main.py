a837d0b2101134047a4ed8a0d37badebb4f03133
OfficialSpade000 <46589135+OfficialSpade000@users.noreply.github.com>
2019-04-23 07:34:22 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,62 @@
+import sys
+from cv_dev import ACTION
+import random
+import urllib.request
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+DEFAULT_USER_AGENTS = [
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 7.0; Moto G (5) Build/NPPS25.137-93-8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.137 Mobile Safari/537.36",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0_4 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11B554a Safari/9537.53",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:59.0) Gecko/20100101 Firefox/59.0",
+    "Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0"
+]
+
+DEFAULT_USER_AGENT = random.choice(DEFAULT_USER_AGENTS)
+
+print(DEFAULT_USER_AGENT)
+file_name = 'p.png'
+
+# Varibles
+
+driver = webdriver.Chrome()
+driver.get('http://google.com')
+xpath = '//*[@id="hplogo"]'
+
+# Execution of process
+
+try:
+
+    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath)))
+    logo = driver.find_element_by_xpath(xpath)
+    logo.click()
+    src = logo.get_attribute('src')
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-agent', DEFAULT_USER_AGENT)]
+    urllib.request.install_opener(opener)
+    urllib.request.urlretrieve(src, file_name)
+
+    driver.quit()
+
+except:
+
+    print('Something went wrong')
+
+    driver.quit()
+    sys.exit(-1)
+
+else:
+
+    try:
+
+        ACTION(file_name)
+
+    except:
+
+        sys.exit(-1)
