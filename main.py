808add2dc68a9df04ace149fbb4ef30a8296effe
Al-buharie <46589135+OfficialSpade000@users.noreply.github.com>
2019-05-11 19:48:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
 import cv2
 import sys
-import random
-import urllib.request
+try:
+    import cloudscraper
+    scraper = cloudscraper.create_scraper()
+except:
+    import cfscrape
+    scraper = cfscrape.create_scraper()
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-DEFAULT_USER_AGENTS = [
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36",
-    "Mozilla/5.0 (Linux; Android 7.0; Moto G (5) Build/NPPS25.137-93-8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.137 Mobile Safari/537.36",
-    "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0_4 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11B554a Safari/9537.53",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:59.0) Gecko/20100101 Firefox/59.0",
-    "Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0"
-]
-
-DEFAULT_USER_AGENT = random.choice(DEFAULT_USER_AGENTS)
-
-print(DEFAULT_USER_AGENT + "\n")
 
 ERROR = """chromedriver.exe was not found or is not compatible with your browser.
 
@@ -28,16 +17,14 @@ Make sure that it's located in your PATH or in your directory.
 Download the driver from this website http://chromedriver.chromium.org if you haven't."""
 
 # Remove this tuple if you want to manually add some.
-webs, xpaths, image_names = 'https://google.com', '//*[@id="hplogo"]', 'image.png'
+webs, xpaths, image_names = 'https://kissanime.ru/Anime/One-Punch-Man-Season-2/Episode-005?id=158021', '//*[@id="formVerify1"]/div[2]/div/div[1]/img', 'image.png'
 
 def catch(web=webs, xpath=xpaths, image_name=image_names):
     try:
         driver = webdriver.Chrome()
-
     except:
         print(ERROR)
         sys.exit()
-
     driver.get(web)
 
     # Execution of process
@@ -45,31 +32,25 @@ def catch(web=webs, xpath=xpaths, image_name=image_names):
     try:
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath)))
         logo = driver.find_element_by_xpath(xpath)
-        logo.click()
+        #logo.click()
         src = logo.get_attribute('src')
-        opener = urllib.request.build_opener()
-        opener.addheaders = [('User-agent', DEFAULT_USER_AGENT)]
-        urllib.request.install_opener(opener)
-        urllib.request.urlretrieve(src, image_name)
-
-        driver.quit()
+        i_mage = scraper.get(src)
+        with open(image_name, "wb") as byte:
+            byte.write(i_mage.content)
 
     except:
         print('Something went wrong')
 
         driver.quit()
-        sys.exit(-1)
+        sys.exit()
 
-    try:
-        action()
 
-    except:
-        print('Wrong call_cv module!')
-        sys.exit(-1)
+    action()
+
 
 def action(name=image_names):
     img = cv2.imread(name, 1)
-    cv2.imshow('some', img)
+    cv2.imshow("!kistack!", img)
     cv2.waitKey(0)
 
 catch()
